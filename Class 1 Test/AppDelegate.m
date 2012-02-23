40501f01a968447e1008c26bd5450aca97ab9edd
jruberg <rubergly@gmail.com>
2012-02-23 18:54:36 -0500

    Initial Commit

---
+++ Class 1 Test/AppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.m
+//  Class 1 Test
+//
+//  Created by Jeffrey Ruberg on 2/23/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
